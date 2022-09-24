3f996693fdd8b8f2d6402d547d3e2f7645c4c078
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-23 20:44:09 -0600

    feat: attributes test

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,9 +1,24 @@
 require_relative './spec_helper'
 
 RSpec.describe NightWriter do
-  it 'exists' do
-    writer = NightWriter.new('message.txt', 'braille.txt')
-    
-    expect(writer).to be_a(NightWriter)
+  before(:each) do
+    @writer = NightWriter.new('message.txt', 'braille.txt')
   end
+
+    it 'exists' do  
+      expect(@writer).to be_a(NightWriter)
+    end
+
+    it 'has attributes' do
+      expect(@writer.input).to eq('message.txt')
+      expect(@writer.output).to eq('braille.txt')
+    end
+
+  # describe '#message' do
+  #   it 'can return a dynamic message' do
+  #     @writer.message
+
+  #     expect(@writer.message).to eq("Created #{ARGV[1]} containing #{capitalize_text.length} characters")
+  #   end
+  # end
 end
