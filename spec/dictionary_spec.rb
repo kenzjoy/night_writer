66c0149f2c3d249d797cae8bdabb852d1cb482df
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-24 00:27:52 -0600

    feat: test alpha data type

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Dictionary do
     it 'exists' do 
       expect(@dictionary).to be_a(Dictionary)
     end
+
+    it 'has attributes' do
+      expect(@dictionary.alpha).to be_a(Hash)
+    end
+
+  describe '#alpha' do
+    it 'can look up an English letter and find its Braille equivalent' do
+      expect(@dictionary.alpha['a']).to eq('0.....')
+      expect(@dictionary.alpha['b']).to eq('0.0...')
+    end
+  end
 end
