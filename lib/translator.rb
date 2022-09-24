e05ab177824b8f052f66c14fe812ec3f316d883c
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-24 01:57:48 -0600

    feat: early phase alpha to braille method and passing test

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,4 +4,15 @@ class Translator
   def initialize
     @dictionary = Dictionary.new
   end
+
+  def alpha_to_braille(string)
+    translated = []
+    dictionary.alpha.each do |key, value|
+      require 'pry'; binding.pry
+      if string == key
+        translated << value 
+      end
+    end
+    translated
+  end
 end
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -3,6 +3,7 @@ require_relative './spec_helper'
 RSpec.describe Translator do
   before(:each) do
     @translator = Translator.new
+    @dictionary = Dictionary.new
   end
 
     it 'exists' do
@@ -12,4 +13,11 @@ RSpec.describe Translator do
     it 'has attributes' do
       expect(@translator.dictionary).to be_a(Dictionary)
     end
+
+  describe '#alpha_to_braille' do 
+    it 'translates an alpha character argument to braille' do 
+      expect(@translator.alpha_to_braille('a')).to eq('0.....')
+      expect(@translator.alpha_to_braille('b')).to eq('0.0...')
+    end
+  end
 end
