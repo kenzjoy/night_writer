9ea215e125a7212721e105b176566d617a27b973
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-26 20:48:23 -0600

    feat: passing stacked_braille method and test

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -7,10 +7,12 @@ reader_file = File.open(ARGV[0], "r")
 incoming_text = reader_file.read
 reader_file.close
 
-braille = translator.alpha_to_braille(incoming_text)
+braille = translator.stacked_braille(incoming_text)
 
 writer_file = File.open(ARGV[1], "w")
 writer_file.write(braille)
 writer_file.close
 
 puts "Created '#{ARGV[1]}' containing #{incoming_text.length} characters"
+
+# ruby ./lib/night_writer.rb message.txt braille.txt
--- lib/translator.rb
+++ lib/translator.rb
@@ -6,23 +6,24 @@ class Translator
   end
 
   def alpha_to_braille(string)
-    translated = []
+    translated = ""
     dictionary.alpha.each do |key, value|
       if string == key
         translated << value 
       end
     end
-    translated.shift
+    translated
   end
 
-  # def stacked_braille(string)
-  #   stack_1 = ""
-  #   stack_2 = ""
-  #   stack_3 = ""
-  #   string.chars.each do |char|
-  #     alpha[char][0..1]
-
-  #     end
-  #   end
-  # end
+  def stacked_braille(string)
+    stack_1 = ""
+    stack_2 = ""
+    stack_3 = ""
+    stack_1 << alpha_to_braille(string).chars[0..1].join
+    stack_2 << alpha_to_braille(string).chars[2..3].join
+    stack_3 << alpha_to_braille(string).chars[4..5].join
+    "#{stack_1}\n" + "#{stack_2}\n" + "#{stack_3}"
+# require 'pry'; binding.pry
+      
+  end
 end
