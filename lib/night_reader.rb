18a95cfe2fbab6c09382240db48fb979fa8c60cc
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-27 17:30:27 -0600

    feat: final commit on translator class before submitting, added notes where I fell short

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -4,10 +4,10 @@ require './lib/translator'
 translator = Translator.new
 
 braille_file = File.open(ARGV[0], "r")
-incoming_braille = braille_file.read
+incoming_str = braille_file.read
 braille_file.close
 
-to_english = translator.translated(incoming_braille)
+to_english = translator.braille_to_alpha(incoming_str)
 
 writer_file = File.open(ARGV[1], "w")
 writer_file.write(to_english)
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -7,6 +7,12 @@ reader_file = File.open(ARGV[0], "r")
 incoming_text = reader_file.read
 reader_file.close
 
+# if incoming_text > 80
+#   braille = translator.line_control(incoming_text)
+# else
+#   braille = translator.translated(incoming_text)
+# end
+
 braille = translator.translated(incoming_text)
 
 writer_file = File.open(ARGV[1], "w")
