56a335db7bb16a16554900722f9311fc89b0f6f8
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-27 13:11:09 -0600

    feat: write test for line_control and empty method

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,24 +6,8 @@ class Translator
   end
 
   def alpha_to_braille(letter)
-    translated = ""
-    dictionary.alpha.each do |key, value|
-      if letter == key
-        translated << value 
-      end
-    end
-    translated
+    dictionary.alpha[letter]
   end
-
-  # def stacked_braille(letter)
-  #   stack_1 = ""
-  #   stack_2 = ""
-  #   stack_3 = ""
-  #   stack_1 << alpha_to_braille(letter).chars[0..1].join
-  #   stack_2 << alpha_to_braille(letter).chars[2..3].join
-  #   stack_3 << alpha_to_braille(letter).chars[4..5].join
-  #   "#{stack_1}\n" + "#{stack_2}\n" + "#{stack_3}"      
-  # end
 
   def translated(incoming_text)
     stack_1 = ""
@@ -35,5 +19,9 @@ class Translator
       stack_3 << alpha_to_braille(letter)[4..5]
     end
     [stack_1, stack_2, stack_3].join("\n")
+  end
+
+  def line_control(incoming_text)
+    
   end
 end
